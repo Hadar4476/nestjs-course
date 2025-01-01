f76ad560beaed7b11547dd14635810282a22b100
Hadar4476 <hadar4476@gmail.com>
2025-01-01 16:49:52 +0200

    changes

---
+++ 08-e2e-testing/src/users/users.controller.ts
@@ -0,0 +1,111 @@
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Param,
+  Query,
+  NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+  Session,
+  UseGuards,
+} from '@nestjs/common';
+
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+
+import { RegisterUserDto } from './dtos/register-user.dto';
+import { LoginUserDto } from './dtos/login-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+
+import {
+  Serialize,
+  SerializeInterceptor,
+} from '../interceptors/serialize.interceptor';
+
+import { UserDto } from 'src/users/dtos/user.dto';
+
+import { CurrentUser } from './decorators/current-user.decorator';
+
+import { User } from './user.entity';
+
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+
+import { AuthGuard } from 'src/guards/auth.guard';
+
+@Controller('auth')
+@Serialize(UserDto)
+export class UsersController {
+  constructor(
+    public usersService: UsersService,
+    private authService: AuthService,
+  ) {}
+
+  @Get('/colors/:color')
+  setColor(@Param('color') color: string, @Session() session: any) {
+    session.color = color;
+  }
+
+  @Get('/colors')
+  getColor(@Session() session: any) {
+    return session.color;
+  }
+
+  @Post('/register')
+  async register(@Body() body: RegisterUserDto, @Session() session: any) {
+    const user = await this.authService.register(body.email, body.password);
+
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('/login')
+  async login(@Body() body: LoginUserDto, @Session() session: any) {
+    const user = await this.authService.login(body.email, body.password);
+
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('/logout')
+  logout(@Session() session: any) {
+    session.userId = null;
+  }
+
+  @Get('/whoami')
+  @UseGuards(AuthGuard)
+  whoAmI(@CurrentUser() currentUser: User) {
+    return currentUser;
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    console.log('handler is running');
+
+    const user = this.usersService.findOne(+id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(+id, body);
+  }
+}
---
+++ 08-e2e-testing/src/users/users.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UsersController } from './users.controller';
+
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+
+import { User } from './user.entity';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [
+    UsersService,
+    AuthService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor,
+    },
+  ],
+})
+export class UsersModule {}
